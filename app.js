f51af71521c46c553105b40f97aa5ed53154bb3a
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-07-05 19:15:42 +0900

    Add 'addEventListener' to change color od div. Still not working.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mainContainer = document.getElementById("container-div");
+const rowDiv = document.getElementById("row-div");
 
 function container(row, col) {
     document.documentElement.style.setProperty('--grid-row', row);
@@ -6,9 +7,14 @@ function container(row, col) {
     for(let i = 0; i < (row * col); i++) {
         const elementRow = document.createElement("div");
         elementRow.classList.add("row");
+        elementRow.setAttribute("id", "row-div");
         mainContainer.appendChild(elementRow);
         
     }
+    rowDiv.addEventListener("mouseover", function(e) {
+        e.setAttribute("style", "color: red");
+    })
     }
 
 container(16, 16);
+
