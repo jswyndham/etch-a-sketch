bee834435d6e347d768670d1e2e442ab9966068f
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-07-06 23:05:07 +0900

    Added pen feature turned on with button.

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ function container(row, col) {
     document.documentElement.style.setProperty('--grid-row', row);
     document.documentElement.style.setProperty('--grid-col', col);
     for(let i = 0; i < (row * col); i++) {
+        //Firstly, created the dynamic div grid.
         const elementRow = document.createElement("div");
         elementRow.classList.add("row");
         elementRow.setAttribute("id", "row-div");
         mainContainer.appendChild(elementRow);
-        
+
+        //Secondly, create a button that turns on the pen feature.
+        btnOne.addEventListener('click', (e)=> {
+            elementRow.addEventListener("mouseover", ()=> {
+        elementRow.style.backgroundColor = "red";
+        console.log(elementRow);
+        })
+    });
     }
-    
 }
 
-container(32, 32)
+container(64, 64)
 
-let mouseDown = false;
-document.body.onmousedown = function() {
-    mouseDown = true;
-}
-document.body.onmouseup = function(){
-    mouseDown = false
-}
 
 
 
