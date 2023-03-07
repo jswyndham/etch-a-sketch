444cee64075462655d8aaa88453bef65941110a6
Doc-Jebus <saunders_wyndham@hotmail.com>
2023-03-07 13:18:42 +0900

    Grid size button functionality added

--- app.js
+++ app.js
@@ -5,84 +5,77 @@ const btnEraser = document.getElementById("btn-eraser");
 const btnRainbow = document.getElementById("btn-rainbow");
 const btnShadow = document.getElementById("btn-shadow");
 const btnClearAll = document.getElementById("btn-clear-all");
-const slider = document.getElementById("myRange");
-const currentValue = document.getElementById("current-value");
-
+const gridBtnSixteen = document.getElementById("grid-sixteen");
+const gridBtnThirtyTwo = document.getElementById("grid-thirty-two");
+const gridBtnFortyEight = document.getElementById("grid-forty-eight");
 
 function pixelContainer(num) {
-    document.documentElement.style.setProperty('--grid-row', num);
-    document.documentElement.style.setProperty('--grid-col', num);
-    for(let i = 0; i < (num * num); i++) {
-        
-        //Firstly, created the dynamic div grid.
-        const elementRow = document.createElement("div");
-        elementRow.classList.add("row");
-        elementRow.setAttribute("id", "row-div");
-        elementRow.style.backgroundColor= "white";
-        mainContainer.appendChild(elementRow);
-        console.log(elementRow);
+  document.documentElement.style.setProperty("--grid-row", num);
+  document.documentElement.style.setProperty("--grid-col", num);
+  for (let i = 0; i < num * num; i++) {
+    //Firstly, created the dynamic div grid.
 
-        //The variables needed for the rainbow pen.
-        let color1 = Math.floor(Math.random() * 256);
-        let color2 = Math.floor(Math.random() * 256);
-        let color3 = Math.floor(Math.random() * 256);
-                
-        //Secondly, buttons that turns on the pen feature, plus clear all.
-        btnBlack.addEventListener('click', ()=> {
-            elementRow.addEventListener("mouseover", ()=> {
-                elementRow.style.backgroundColor = "black";
-                console.log(btnBlack);
-        });
-        });
+    const elementRow = document.createElement("div");
+    elementRow.classList.add("row");
+    elementRow.setAttribute("id", "row-div");
+    elementRow.style.backgroundColor = "white";
+    mainContainer.appendChild(elementRow);
 
-        btnEraser.addEventListener('click', ()=> {
-            elementRow.addEventListener("mouseover", ()=> {
-                elementRow.style.backgroundColor = "white";
-                console.log(elementRow);
-        });
-        });
+    //The variables needed for the rainbow pen.
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
 
-        btnRainbow.addEventListener('click', ()=> {
-            elementRow.addEventListener("mouseover", ()=> {
-                elementRow.style.backgroundColor = `rgba(${color1},${color2},${color3})`;
-                console.log(elementRow);
-        })});
+    //Secondly, buttons that turns on the pen feature, plus clear all.
+    btnBlack.addEventListener("click", () => {
+      elementRow.addEventListener("mouseover", () => {
+        elementRow.style.backgroundColor = "black";
+      });
+    });
 
-        btnShadow.addEventListener('click', ()=> {
-            elementRow.addEventListener("mouseover", ()=> {
-                elementRow.style.backgroundColor = `rgba(0, 0, 0, .2)`;
-                console.log(elementRow);
-        });
-        });
+    btnEraser.addEventListener("click", () => {
+      elementRow.addEventListener("mouseover", () => {
+        elementRow.style.backgroundColor = "white";
+      });
+    });
 
-        btnClearAll.addEventListener('click', ()=> {
-            elementRow.style.backgroundColor = "white";
-            console.log(btnClearAll);
-        });
+    btnRainbow.addEventListener("click", () => {
+      elementRow.addEventListener("mouseover", () => {
+        elementRow.style.backgroundColor = `rgba(${color1},${color2},${color3})`;
+      });
+    });
 
-        
-    }
-    
+    btnShadow.addEventListener("click", () => {
+      elementRow.addEventListener("mouseover", () => {
+        elementRow.style.backgroundColor = `rgba(0, 0, 0, .2)`;
+      });
+    });
+
+    btnClearAll.addEventListener("click", () => {
+      elementRow.style.backgroundColor = "white";
+    });
+  }
 }
 
+pixelContainer(gridBtnSixteen.value);
 
-function sliderAdjust() {
-            let value = parseInt(slider.value);
-            currentValue.innerText = value;
-                pixelContainer(value);
-                console.log(value);
-            
-        }
-slider.addEventListener('change', sliderAdjust); 
+gridBtnSixteen.addEventListener("click", (e) => {
+  while (mainContainer.lastElementChild) {
+    mainContainer.removeChild(mainContainer.lastElementChild);
+  }
+  pixelContainer(e.target.value);
+});
 
-pixelContainer(slider.min);
+gridBtnThirtyTwo.addEventListener("click", (e) => {
+  while (mainContainer.lastElementChild) {
+    mainContainer.removeChild(mainContainer.lastElementChild);
+  }
+  pixelContainer(e.target.value);
+});
 
-
-
-
-
-
-
-
-
-
+gridBtnFortyEight.addEventListener("click", (e) => {
+  while (mainContainer.lastElementChild) {
+    mainContainer.removeChild(mainContainer.lastElementChild);
+  }
+  pixelContainer(e.target.value);
+});
